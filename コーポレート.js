735ad77a827a56cfbc9201c1cb3e203057db50b8
koki800 <koki1995-moominvalley@docomo.ne.jp>
2021-09-20 13:27:46 +0900

    fifthcommit

--- コーポレート.js
+++ コーポレート.js
@@ -1,7 +1,20 @@
 const target = document.getElementsByTagName("input")
-target.addEventListener("mouseover", () => {
- document.getElementsByClassName("tooltip").style.display="block"
+target[1].addEventListener("mouseover", () => {
+  const content = target[1].textContent
+
+  if (content = null) {
+    document.getElementsByClassName("tooltip").style.display = "block"
+  }
 })
-target.addEventListener("mouseleave", () => {
+target[2].addEventListener("mouseover", () => {
+  const content = target[2].textContent
+
+  if (content = null) {
+    document.getElementsByClassName("tooltip").style.display = "block"
+  }else if(content.includes("＠") === false){
+    document.getElementsByClassName("tooltip2").style.display = "block"
+  }
+})
+target[1,2].addEventListener("mouseleave", () => {
   document.getElementsByClassName("tooltip").style.display="none"
 })
