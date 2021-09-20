1cbff7d215060d6aa26b9d32a74ff87990a0aedc
koki800 <koki1995-moominvalley@docomo.ne.jp>
2021-09-20 18:35:24 +0900

    seventhcommit

--- コーポレート.js
+++ コーポレート.js
@@ -1,23 +1,29 @@
 const target = document.getElementsByTagName("input");
-target[1].addEventListener("mouseover", setTimeout(() => {
+
+const func = () => {
   const content = target[1].textContent;
 
   if (content == null) {
     document.getElementsById("tooltip").style.display = "block"
   };
-},1000));
-target[2].addEventListener("mouseover", setTimeout(() => {
-  const content = target[2].textContent
+};
+target[1].addEventListener("mouseover", setTimeout(func(), 1000));
 
-  if (content = null) {
+const func1 = () => {
+  const content = target[2].textContent;
+
+  if (content == null) {
     document.getElementsById("tooltip").style.display = "block"
-  }else if(content.includes("＠") == false){
+  } else if (content.includes("@") == false) {
     document.getElementsById("tooltip2").style.display = "block";
 
     const span = document.getElementsById("tooltip2_text").firstChild;
-    const span.textContent = content;
-  }
-},1000))
-target[1, 2].addEventListener("mouseleave", () => {
+    span.textContent = content;
+  };
+};
+target[2].addEventListener("mouseover", setTimeout(func1(), 1000));
+
+const func2 = () => {
   document.getElementsByClassName("tooltip").style.display = "none";
-});
+};
+target[1, 2].addEventListener("mouseleave", setTimeout(func2(),1000));
