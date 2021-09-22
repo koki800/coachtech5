91166252ebd2159920988fd45b5238428feb1897
koki800 <koki1995-moominvalley@docomo.ne.jp>
2021-09-22 20:25:25 +0900

    eighthcommit

--- コーポレート.js
+++ コーポレート.js
@@ -7,7 +7,8 @@ const func = () => {
     document.getElementsById("tooltip").style.display = "block"
   };
 };
-target[1].addEventListener("mouseover", setTimeout(func(), 1000));
+const setFunc = setTimeout(func, 1000);
+target[1].addEventListener("mouseover",setFunc );
 
 const func1 = () => {
   const content = target[2].textContent;
@@ -21,9 +22,11 @@ const func1 = () => {
     span.textContent = content;
   };
 };
-target[2].addEventListener("mouseover", setTimeout(func1(), 1000));
+const setFunc1 = setTimeout(func1, 1000)
+target[2].addEventListener("mouseover", setFunc1);
 
 const func2 = () => {
   document.getElementsByClassName("tooltip").style.display = "none";
 };
-target[1, 2].addEventListener("mouseleave", setTimeout(func2(),1000));
+const setFunc2 = setTimeout(func2, 1000);
+target[1, 2].addEventListener("mouseleave",setFunc2);
