cd502c89c0a6da3166987aab66cd6bea2f44e1e7
koki800 <koki1995-moominvalley@docomo.ne.jp>
2021-09-23 15:05:11 +0900

    eleventhcommit

--- コーポレート.js
+++ コーポレート.js
@@ -2,39 +2,39 @@ const target = document.getElementsByTagName("input");
 
 // 名前のツールチップ
 
-const func = () => {
+const func = (e) => {
   const content = target[1].textContent;
 
   if (content.length == 0) {
     document.getElementById("tooltip").style.display = "block";
-    document.getElementById("tooltip").style.top = e.clientY;
-    document.getElementById("tooltip").style.left = e.clientX;
+    document.getElementById("tooltip").style.top = e.clientY + "px";
+    document.getElementById("tooltip").style.left = e.clientX + "px";
   };
 };
 const setFunc = (e) => {
-  setTimeout(func, 1000)
+  setTimeout(func, 500,e)
 };
-target[1].addEventListener("mouseover",setFunc );
+target[1].addEventListener("mouseover",setFunc);
 
 // メールアドレスのツールチップ
 
-const func1 = () => {
+const func1 = (e) => {
   const content = target[2].textContent;
 
   if (content.length == 0) {
     document.getElementById("tooltip").style.display = "block";
-    document.getElementById("tooltip").style.top = e.clientY;
-    document.getElementById("tooltip").style.left = e.clientX;
+    document.getElementById("tooltip").style.top = e.clientY + "px";
+    document.getElementById("tooltip").style.left = e.clientX + "px";
   } else if (content.includes("@") == false) {
     document.getElementById("tooltip2").style.display = "block";
-    document.getElementById("tooltip2").style.top = e.clientY;
-    document.getElementById("tooltip2").style.left = e.clientX;
+    document.getElementById("tooltip2").style.top = e.clientY + "px";
+    document.getElementById("tooltip2").style.left = e.clientX + "px";
     const span = document.getElementById("tooltip2_text").firstChild;
     span.textContent = content;
   };
 };
 const setFunc1 = (e) => {
-  setTimeout(func1, 1000)
+  setTimeout(func1, 500,e)
 }
 target[2].addEventListener("mouseover", setFunc1);
 
@@ -44,6 +44,6 @@ const func2 = () => {
   document.getElementsByClassName("tooltip").style.display = "none";
 };
 const setFunc2 = () => {
-  setTimeout(func2, 1000)
+  setTimeout(func2, 500)
 };
 target[1, 2].addEventListener("mouseleave",setFunc2);
